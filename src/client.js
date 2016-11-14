e243c615f6b7d71b9fa90db019ba811a929dd6eb
Carroll Yu <carrollcyu@gmail.com>
2016-11-14 05:35:21 -0800

    syntax

--- src/client.js
+++ src/client.js
@@ -3,4 +3,4 @@ import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 import routes from './routes'
 
-render(<Router routes={routes} history={browserHistory}/>, content)
+render(<Router routes={routes} history={browserHistory}/>, document.getElementById('content'))
