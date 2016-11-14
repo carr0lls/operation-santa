9a07816b4ef303f9444f59656971072742ecfa26
Carroll Yu <carrollcyu@gmail.com>
2016-11-13 20:06:30 -0800

    bootstrap libraries

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,11 +5,29 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Operation Santa</h1>
-        <ul role="nav">
-          <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
-        </ul>
+        <nav className="navbar navbar-light bg-faded">
+          <a className="navbar-brand" href="#">Operation Santa</a>
+          <ul className="nav navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" onlyActiveOnIndex>Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">About</NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="http://example.com" id="supportedContentDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div className="dropdown-menu" aria-labelledby="supportedContentDropdown">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+          <form className="form-inline float-xs-right">
+            <input className="form-control" type="text" placeholder="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </nav>
         {this.props.children}
       </div>
     )
--- src/server.js
+++ src/server.js
@@ -42,6 +42,10 @@ function renderPage(appHtml) {
       <head>
         <meta charSet="utf-8" />
         <link href="http://fonts.googleapis.com/css?family=Raleway:100" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous" />
+		<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+		<script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
         <link href="css/styles.css" rel="stylesheet" type="text/css" />
       </head>
       <body>
