7f35120d05bcf64b606a682021568bef9eaf387e
Carroll Yu <carrollcyu@gmail.com>
2016-11-15 05:00:00 -0800

    home page family listings

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,30 +4,49 @@ import React from 'react'
 		constructor(props) {
 			super(props)
 			this.state = {
-				data: []
+				users: {}
 			}
 			this.api = {
 				url: props.route.containerData.api.url,
 				refresh: props.route.containerData.api.pollInterval
 			}
 
-			this.renderData = this.renderData.bind(this)
+			this.fetchData = this.fetchData.bind(this)
 			this.submitForm = this.submitForm.bind(this)
 		}
 
-		renderData() {
-
+		fetchData() {
+			$.ajax({
+				url: this.api.url + 'user',
+				type: 'GET'
+			}).done((res) => {
+				this.setState({ users: res })
+			})
 		}
 
 		submitForm() {
 			
 		}
 
+		componentDidMount() {
+			this.fetchData()
+			setInterval(this.fetchData, this.api.refresh)
+		}
+
 		render() {
+			let user, userList = []
+			Object.keys(this.state.users).map((key) => {
+				user = this.state.users[key]
+				userList.push(<li className="list-group-item"><a key={key} href="">{user.first_name} {user.last_name}</a></li>)
+			})
+
 			return (
 				<div className="home container mx-auto p-1 mt-3">
 					<header>
-						<h1 className="header-title animated slideInUp">OPERATION SANTA</h1>
+						<h1 className="header-title animated slideInUp">Operation Santa</h1>
+						<ul className="list-group">
+							{ userList }
+						</ul>
 					</header>
 				</div>
 			)
