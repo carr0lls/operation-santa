f3de44f3a614d2d966ab2406d6b9447ce9b5fc21
Carroll Yu <carrollcyu@gmail.com>
2016-11-17 18:25:56 -0800

    update api endpoint

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,4 +1,4 @@
 export const Constants = {
-  API_FETCH_URL: 'https://operation-santa.herokuapp.com/api/',
+  API_FETCH_URL: 'https://api-operation-santa.herokuapp.com/api/',
   API_REFRESH_INTERVAL: 30000
 }
