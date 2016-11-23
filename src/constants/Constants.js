11bafcf732cdf6f5f26ae965d67af693bffe2302
Carroll Yu <carrollcyu@gmail.com>
2016-11-23 03:26:29 -0800

    change auth to use sess tokens and hydrate user data on componentDidMount, refactor navbar rendering process, convert a tags to react-router links on Home page

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,5 +1,5 @@
 export const Constants = {
   API_FETCH_URL: 'http://localhost:3000/api/',
   API_REFRESH_INTERVAL: 30000,
-  LOCAL_STORAGE_PROFILE_COOKIE: 'os.profile'
+  LOCAL_STORAGE_PROFILE_COOKIE: 'os'
 }
