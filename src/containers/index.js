81a3b23064d12bc9fe62112af783e9c72491aa99
Carroll Yu <carrollcyu@gmail.com>
2016-12-04 18:51:57 -0800

    edit profile route

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,7 @@
 export About from './About/About'
 export Account from './Account/Account'
 export App from './App/App'
+export EditProfile from './EditProfile/EditProfile'
 export Home from './Home/Home'
 export Login from './Login/Login'
 export Profile from './Profile/Profile'
