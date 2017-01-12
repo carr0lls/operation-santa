8532c569cda53a9ba4b2de2863a58b39ec6c5c59
Carroll Yu <carrollcyu@gmail.com>
2017-01-11 22:35:57 -0800

    convert react components to stateless functional components

--- src/components/DonorAccountRegisterForm.js
+++ src/components/DonorAccountRegisterForm.js
@@ -1,52 +1,35 @@
-import React from 'react'
+import React from 'react';
 
-export default class DonorAccountRegisterForm extends React.Component {
-	constructor() {
-		super()
-		this.handleSubmit = this.handleSubmit.bind(this)
-	}
-	handleSubmit(e) {
-		e.preventDefault()
-		let first_name = this.refs.first_name.value.trim()
-		let last_name = this.refs.last_name.value.trim()
-		let username = this.refs.username.value.trim()
-		let password = this.refs.password.value.trim()
-		let phone_number = this.refs.phone_number.value.trim()
-		let account_type = this.refs.account_type.value.trim()
+const DonorAccountRegisterForm = ({className, onSubmitForm, onUpdateForm}) => {
 
-		let formData = { first_name, last_name, username, password, phone_number, account_type }
-		this.props.onSubmitForm(formData)
-	}
+	return (
+		<div className={className}>
+			<h4>Donate Christmas gifts to families in need!</h4>
+			<form onSubmit={onSubmitForm} onChange={onUpdateForm}>
+				<div className="form-group">
+			    	<label htmlFor="first_name">First Name</label>
+			    	<input type="text" className="form-control" name="first_name" placeholder="Enter first name" required />
+				</div>
+				<div className="form-group">
+			    	<label htmlFor="last_name">Last Name</label>
+			    	<input type="text" className="form-control" name="last_name" placeholder="Enter last name" required />
+				</div>
+				<div className="form-group">
+			    	<label htmlFor="username">Email address</label>
+			    	<input type="email" className="form-control" name="username" aria-describedby="emailHelp" placeholder="Enter email" required />
+				</div>
+				<div className="form-group">
+			    	<label htmlFor="password">Password</label>
+			    	<input type="password" className="form-control" name="password" placeholder="Password" required />
+				</div>
+				<div className="form-group">
+					<label htmlFor="phone-number" className="form-control-label">Phone Number</label>
+					<input type="tel" className="form-control" name="phone_number" placeholder="(888)888-8888" required />
+				</div>
+				<button type="submit" className="btn btn-danger">Submit</button>
+			</form>
+		</div>
+	);
+};
 
-	render() {
-		return (
-			<div className={this.props.className} onSubmit={this.handleSubmit}>
-				<h4>Donate Christmas gifts to families in need!</h4>
-				<form>
-					<div className="form-group">
-				    	<label htmlFor="first_name">First Name</label>
-				    	<input type="text" className="form-control" ref="first_name" name="first_name" placeholder="Enter first name" required />
-					</div>
-					<div className="form-group">
-				    	<label htmlFor="last_name">Last Name</label>
-				    	<input type="text" className="form-control" ref="last_name" name="last_name" placeholder="Enter last name" required />
-					</div>
-					<div className="form-group">
-				    	<label htmlFor="username">Email address</label>
-				    	<input type="email" className="form-control" ref="username" name="username" aria-describedby="emailHelp" placeholder="Enter email" required />
-					</div>
-					<div className="form-group">
-				    	<label htmlFor="password">Password</label>
-				    	<input type="password" className="form-control" ref="password" name="password" placeholder="Password" required />
-					</div>
-					<div className="form-group">
-						<label htmlFor="phone-number" className="form-control-label">Phone Number</label>
-						<input type="tel" className="form-control" ref="phone_number" name="phone_number" placeholder="(888)888-8888" required />
-					</div>
-					<input type="hidden" ref="account_type" name="account_type" value="donor" />
-					<button type="submit" className="btn btn-danger">Submit</button>
-				</form>
-			</div>
-		)
-	}
-}
+export default DonorAccountRegisterForm;
