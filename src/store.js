87a6e89ec4f64a17cf8cd24e0f7e1dd84a8e01fd
Carroll Yu <carrollcyu@gmail.com>
2017-11-26 17:00:20 -0800

    remove old packages

--- src/store.js
+++ src/store.js
@@ -1,17 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { routerMiddleware } from 'react-router-redux';
+import history from './history';
+import reducers from './reducers';
 import reduxPromise from 'redux-promise';
 import reduxThunk from 'redux-thunk';
-import logger from 'redux-logger';
-import history from './history';
-import { routerMiddleware } from 'react-router-redux';
-import reducers from './reducers';
 
 const reactRouterMiddleware = routerMiddleware(history);
 
 export default createStore(
 	reducers,
 	composeWithDevTools(
-		applyMiddleware(reactRouterMiddleware, reduxPromise, reduxThunk, logger)
+		applyMiddleware(reactRouterMiddleware, reduxPromise, reduxThunk)
 	),
 );
